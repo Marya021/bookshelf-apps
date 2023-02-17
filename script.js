f8646de738bfa1801d7164a6c4843ccff661bbb8
Marya021 <maryabatubara0202@gmail.com>
2023-02-17 20:28:42 +0700

    Script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const localStorage = "BOOKS_DATA"
+
+const title = document.querySelector('#inputBookTitle')
+const errortitle = document.querySelector('#errorTitle')
+
+
+const author = document.querySelector('#inputBookAuthor')
+const errorauthor = document.querySelector('#errorAuthor')
+
+const year = document.querySelector('#inputBookYear')
+const erroryear = document.querySelector('#errorYear')
+
+const read = document.querySelector('#inputBookIsComplete')
+
+const btnsubmit = document.querySelector('#bookSubmit')
+
+const searchbook = document.querySelector('#search_section')
+
+const btnsearch = document.querySelector('#searchSubmit')
+
+
+
+btnsubmit.addEventListener("click")
